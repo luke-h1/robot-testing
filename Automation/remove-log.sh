120d2dc22c8c013c23766bba038f3c363eb7ba33
luke-h1 <luke.howsam@yahoo.com>
2020-09-15 21:19:01 +0100

    remove safety warnings. keep it simple

--- Automation/remove-log.sh
+++ Automation/remove-log.sh
@@ -5,27 +5,6 @@
 # the below will cd into each sub dir and get rid of all xml, html, png & .log files.
 
 
-echo "Do Not run this script blindly"
-echo "you'll probably regret it"
-echo "this script will delete all robot fw"
-echo "logs. Make sure you do not need them"
-echo "before running"
-sleep 2
-
-CONTINUE=false
-echo ""
-echo "Have you read through the script you're about to run and "
-echo "understood that it will delete all of your test logs (y/n) ? "
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    echo "Deleting all logging now... "
-    CONTINUE=true
-fi
-
-if ! $CONTINUE; then
-    echo "Please go read the script, it only takes a few minutes 💻"
-    exit
-fi
 
 
 for dir in ./*/
